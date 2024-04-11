adac24d7249e5616625bf0d0a86fa82aedab36b1
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-04-11 14:44:57 -0700

    contact component is completed

---
+++ components/contact.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+import { sendEmail } from "@/actions/sendEmail";
+import { useFormStatus } from "react-dom";
+import SubmitButton from "./submitButton";
+import toast from "react-hot-toast";
+
+export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+  const { pending } = useFormStatus();
+  return (
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+    >
+      <SectionHeading>Contact me</SectionHeading>
+
+      <p className="text-gray-700 -mt-6">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:anitanasimi79@gmail.com">
+          anitanasimi79@gmail.com
+        </a>{" "}
+        or via this form
+      </p>
+      <form
+        className="mt-10 flex flex-col dark:text-black"
+        action={async (formData) => {
+         const {data, error } = await sendEmail (formData)
+         if (error) {
+        toast.error(error)
+          return;
+         }
+        toast.success("Email sent successfully")
+        }}
+      >
+        <input
+          className="px-4 h-14 round-lg borderBlack"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={80}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 "
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        ></textarea>
+      <SubmitButton></SubmitButton>
+      </form>
+    </motion.section>
+  );
+}
