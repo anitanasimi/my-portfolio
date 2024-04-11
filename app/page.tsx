afbcb49981e4ecfbc353e34fb2af81a25c942b1d
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-04-11 15:10:13 -0700

    dark mode added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-devider";
+import About from "@/components/about";
+import Projects from "@/components/projects";
+import Skills from "@/components/skills";
+import Contact from "@/components/contact";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center px-4">
       <Intro />
+    <SectionDivider/>
+    <About/>
+    <Projects/>
+    <Skills/>
+    <Contact/>
     </div>
   );
 }
