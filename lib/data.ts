eb1908b03e5e31afd35156587febff65c9f04f02
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2024-04-11 16:21:03 -0700

    data.ts file added

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-// import corpcommentImg from "@/public/corpcomment.png";
-// import rmtdevImg from "@/public/rmtdev.png";
-// import wordanalyticsImg from "@/public/wordanalytics.png";
+import comingSoon from "@/public/coming-soon.jpg";
+
 
 export const links = [
   {
@@ -33,3 +32,49 @@ export const links = [
   },
 ] as const;
 
+export const projectsData = [
+  {
+    title: "Cocoon",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
+    tags: ["React", "Next.js", "Tailwind", "more"],
+    imageUrl: "/public/coming-soon.jpg",
+  },
+  {
+    title: "coming soon",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    tags: ["React", "Next.js", "Tailwind", "more"],
+
+    imageUrl: "/public/coming-soon.jpg",
+  },
+  {
+    title: "Coming soon",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tags: ["React", "Next.js", "Tailwind", "more"],
+    imageUrl: "/public/coming-soon.jpg",
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
